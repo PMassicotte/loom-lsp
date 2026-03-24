63ccde9c9208511c98db31b861520432f41d5fc1
Philippe Massicotte <pmassicotte@hotmail.com>
2026-03-24 11:27:18 -0400

    refactor(main.rs): use CARGO_PKG_VERSION for dynamic versioning in ServerInfo

--- crates/loom/src/main.rs
+++ crates/loom/src/main.rs
@@ -13,7 +13,7 @@ impl LanguageServer for LoomServer {
         Ok(InitializeResult {
             server_info: Some(ServerInfo {
                 name: "loom".to_string(),
-                version: Some("0.1.0".to_string()),
+                version: Some(env!("CARGO_PKG_VERSION").to_string()),
             }),
             capabilities: ServerCapabilities::default(),
         })
